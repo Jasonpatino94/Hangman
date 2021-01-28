6c3bb22ce18ee1fd35fdb53cce9923957d22e6a0
Jason <Jason.patino94@gmail.com>
2021-01-28 04:23:27 -0500

    put in lots of function

--- frontend/hangman.js
+++ frontend/hangman.js
@@ -1,13 +1,37 @@
 let vehicles = ["accord", "corolla", "civic", "versa", "maxima", "titan", "highlander", "bronco", "mustang"]
 let answer = ""
 const keys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
- 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
- let guessedWord = null
+let wordToGuess = null
+let guessedLetters = []
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+let totalSeconds = 0
+let reset = document.getElementById('reset')
+let mistakes = 0
+let wrongGuesses = document.getElementsByClassName('wrongGuesses')[0]
+let maxWrongGuesses = document.getElementById("maxWrongGuesses")
+const keyboard = document.getElementsByClassName('keyboard')[0]
 
 
+// gives you the max number of wrong guesses
+maxWrongGuesses.innerHTML = 5
 
 
+// resets the timer and gives you a new word
+reset.addEventListener("click" , e => {
+    totalSeconds = 0
+    guessedLetters = []
+    mistakes = 0
+    wrongGuesses.innerHTML = mistakes
+    
+    randomVehicle()
+    guessWord()
+    
+    buttons = document.querySelectorAll(".letter")
+    Array.from(buttons).map( button => button.disabled = false)
+})
 
 // picks a random word to guess and replaces each character with underscores
 function randomVehicle(){
@@ -16,46 +40,91 @@ function randomVehicle(){
 
 // create keyboard
 function createButtons(){
-    const keyboard = document.getElementsByClassName('keyboard')[0]
     const buttons = document.createElement('ul')
    
     for (let i = 0; i < keys.length; i++) {
-        buttons.id = 'key'
-        const list = document.createElement('button')
-        list.className = 'letter'
-        list.innerHTML = keys[i]
-        // // this is to make a line brake after 3 and m (just to make it neater)
-        // if (keys[i] === "3" || "m"){
-        //     const lineBreak = document.createElement('br')
-        //     keyboard.appendChild(lineBreak)
-        // } 
+        const button = document.createElement('button')
+        // button.dataset.letter = keys[i]
+        button.className = `letter`
+        button.innerHTML = keys[i]
         keyboard.appendChild(buttons)
-        buttons.appendChild(list)
+        buttons.appendChild(button)
+        button.addEventListener("click", () => makeGuess(button) )
+        }
     }
-}
+    
+    
+    // inputs the word being guessed
+    function guessWord(){
+        wordToGuess = answer.split("").map(letter => "_").join(" ")
+        
+        document.getElementsByClassName('mysteryWord')[0].innerHTML = wordToGuess
+    } 
+    
+    // Choosing a guessed character
+    function makeGuess(button){
+        const character = button.innerHTML
+        guessedLetters.indexOf(character) === -1 ? guessedLetters.push(character) : null
+        button.setAttribute('disabled', true)
+        const charIndex = answer.indexOf(button.innerHTML)
+        if (charIndex >= 0){
+            const splitWord = wordToGuess.split(" ")
+            splitWord[charIndex] = button.innerHTML
+            wordToGuess = splitWord.join(" ")
+            document.getElementsByClassName('mysteryWord')[0].innerHTML = wordToGuess
+            youWin()
+        } else if (charIndex === -1) {
+            mistakes++
+            numberGuessedWrong()
+            youLose()
+        }
+        console.log(wordToGuess)
+        console.log(answer)
+    }
+    
+    // updates how many wrong guesses you had
+    function numberGuessedWrong(){
+        wrongGuesses.innerHTML = mistakes
+    }
+    
+    
+    // Increases timer by 1 per second.
+    function setTime(){
+        totalSeconds++
+        seconds.innerHTML = totalSeconds % 60
+        minutes.innerHTML = Math.floor(totalSeconds / 60)
+    }
+
+    // if you guess all the letters correctly, you will win
+    function youWin(){
+        if (wordToGuess === answer.split('').join(' ')){
+            alert(`you won in ${minutes.innerHTML} minutes and ${seconds.innerHTML} seconds!!`)
+        }
+    }                    
 
 
-// inputs the word being guessed
-function guessWord(){
-    guessedWord = answer.split(' ')
-
-    document.getElementsByClassName('mysteryWord')[0].innerHTML = guessedWord
-} 
-
-// updates how many wrong guesses you had
-
-
-// creates timer
-
-
-
-
-
-
-
-
-
+    // function that will tell you that you lost.
+    function youLose(){
+        if (wrongGuesses.innerHTML === maxWrongGuesses.innerHTML) {
+        //    disable all buttons and put down that you lost.
+        document.getElementsByClassName('mysteryWord')[0].innerHTML = `you lost, the correct answer is <span >${answer}</span>`
+            
+        }
+    }
+     
 randomVehicle();
 createButtons();
 guessWord();
+setInterval(setTime, 1000);
+
+
+
+// make each image pop up for each mistake
+// mulitple letters have to pop up for each mistake
+// have to disable all buttons when you win or lose
+// resets image when you reset the game
+// modal all vehicles in the list
+// modal the scoreboard
+// 
+
 
